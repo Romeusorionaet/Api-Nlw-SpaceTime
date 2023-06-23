25369d270f7329a5021ab8cfcb1fc743baff1486
romeusorionaet <romeusoares14569@gmail.com>
2023-06-23 10:22:24 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { authRoutes } from './routes/auth'
+import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
+
+const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetimme',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+app.register(uploadRoutes)
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log(`🐓Server is running on http://localhost:3333`)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
