34453eb1b8187a59fdb5e212ce4b988a91ee9cfe
romeusorionaet <romeusoares14569@gmail.com>
2023-07-03 11:51:25 -0300

    add pm2

---
+++ ecosystem.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: './src/server.ts',
+      instances: 'max',
+      env: {
+        NODE_ENV: 'developement',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
